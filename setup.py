fa878642ecea26c4d49e2db6108cc22eb9b40dbc
Keshav <keshavgogia57@gmail.com>
2024-07-13 01:52:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 name='mlproject',
 version='0.0.1',
 author='Keshav',
-author_email='keshavgogia57@gmail.com',
+author_email='keshavgogia57x@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
